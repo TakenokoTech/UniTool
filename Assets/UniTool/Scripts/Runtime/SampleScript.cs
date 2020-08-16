8267419e28c38a2ecd8767b99e337b5f060b5164
takenoko <takenoko.tech@gmail.com>
2020-08-17 02:02:06 +0900

    add test.

---
+++ Assets/UniTool/Scripts/Runtime/SampleScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UniTool.Scripts.Runtime
+{
+    public class SampleScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
