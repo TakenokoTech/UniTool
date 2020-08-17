9b5c41badddc3a6a5f3b7988adfb431c25e4f41c
takenoko <takenoko.tech@gmail.com>
2020-08-17 13:52:24 +0900

    add package exporter.

---
+++ Assets/UniTool/Scripts/Editor/UnityPackageExporter.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace UniTool.Scripts.Editor
+{
+    public class UnityPackageExporter : UnityEditor.Editor
+    {
+        private const string Root = "UniTool";
+        
+        [MenuItem("Tools/Export Unitypackage")]
+        public static void Export()
+        {
+            var version = Environment.GetEnvironmentVariable("UNITY_PACKAGE_VERSION");
+            var fileName = string.IsNullOrEmpty(version) ? "UniTool.unitypackage" : $"UniTool.{version}.unitypackage";
+            var exportPath = "./Build/" + fileName;
+            
+            var path = Path.Combine(Application.dataPath, Root);
+            var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
+                .Where(x => Path.GetExtension(x) == ".cs" || Path.GetExtension(x) == ".meta" || Path.GetExtension(x) == ".asmdef")
+                .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
+                .ToArray();
+ 
+            Debug.Log("Export below files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
+ 
+            var dir = new FileInfo(exportPath).Directory;
+            if (dir != null && !dir.Exists) dir.Create();
+            
+            AssetDatabase.ExportPackage(assets, exportPath, ExportPackageOptions.Default);
+ 
+            Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
+        }
+    }
+}
