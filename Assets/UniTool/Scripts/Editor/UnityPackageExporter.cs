95bf6437f797d77df6308c78dac90d48967d3a0a
takenoko <takenoko.tech@gmail.com>
2020-08-24 09:48:00 +0900

    fix packaging flow.

--- Assets/UniTool/Scripts/Editor/UnityPackageExporter.cs
+++ Assets/UniTool/Scripts/Editor/UnityPackageExporter.cs
@@ -9,13 +9,14 @@ namespace UniTool.Scripts.Editor
     public class UnityPackageExporter : UnityEditor.Editor
     {
         private const string Root = "UniTool";
+        private const string DistDir = "Dist";
         
         [MenuItem("Tools/Export Unitypackage")]
         public static void Export()
         {
             var version = Environment.GetEnvironmentVariable("UNITY_PACKAGE_VERSION");
             var fileName = string.IsNullOrEmpty(version) ? "UniTool.unitypackage" : $"UniTool.{version}.unitypackage";
-            var exportPath = "Dist/" + fileName;
+            var exportPath = $"{DistDir}/{fileName}";
             
             var path = Path.Combine(Application.dataPath, Root);
             var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
@@ -29,7 +30,8 @@ namespace UniTool.Scripts.Editor
             if (dir != null && !dir.Exists) dir.Create();
             
             AssetDatabase.ExportPackage(assets, exportPath, ExportPackageOptions.Default);
- 
+            File.AppendAllText($"{DistDir}/version", "0.0.1");
+
             Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
         }
     }
