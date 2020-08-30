02daa23948af4fa8cd9c936dfbacb27ed28eaefe
Takenoko <takenoko.tech@gmail.com>
2020-08-31 01:49:36 +0900

    Update ColorExtensionTest.cs

--- Assets/UniTool/Tests/PlayMode/ColorEx/ColorExtensionTest.cs
+++ Assets/UniTool/Tests/PlayMode/ColorEx/ColorExtensionTest.cs
@@ -11,7 +11,7 @@ namespace UniTool.Tests.PlayMode.ColorEx
         [Test]
         public void ToColorTest()
         {
-            Assert.AreEqual(new Color(0,0,0,1), "#000001".ToColor());
+            Assert.AreEqual(new Color(0,0,0,1), "#000000".ToColor());
             Assert.AreEqual(new Color(1,1,1,1), "#FFFFFF".ToColor());
             
             AssertEx.AreThrow(new ArgumentException($"000000 cannot be converted to color."), () => "000000".ToColor());
