78a662871b75eb244fbf7a53d921547e0fc5c6eb
takenoko <takenoko.tech@gmail.com>
2020-09-28 01:52:34 +0900

    fix test.

--- Assets/UniTool/Event/SimpleDirectory.cs
+++ Assets/UniTool/Event/SimpleDirectory.cs
@@ -1,24 +1,46 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using UniTool.EnumerableEx;
 
 namespace UniTool.Event
 {
     public static class SimpleDirectory
     {
-        /**
-         * ディレクトリの再起削除
-         */
+        /// <summary>
+        /// ディレクトリ作成
+        /// </summary>
+        public static DirectoryInfo CreateDir(string targetDirectoryPath)
+        {
+            return !Directory.Exists(targetDirectoryPath) ? Directory.CreateDirectory(targetDirectoryPath) : null;
+        }
+        
+        /// <summary>
+        /// ディレクトリのファイル名を再起取得
+        /// </summary>
+        public static List<string> GetFileName(string targetDirectoryPath)
+        {
+            var pathList = new List<string>();
+            void GetFileName(string dicPath)
+            {
+                if (!Directory.Exists(dicPath)) return;
+                pathList.AddRange(Directory.GetFiles(dicPath));
+                Directory.GetDirectories(dicPath).Foreach(GetFileName);
+                pathList.Add(dicPath);
+            }
+            GetFileName(targetDirectoryPath);
+            return pathList;
+        }
+        
+        /// <summary>
+        /// ディレクトリの再起削除
+        /// </summary>
         public static void DeleteDir(string targetDirectoryPath)
         {
             if (!Directory.Exists(targetDirectoryPath)) return;
-            foreach (var filePath in Directory.GetFiles(targetDirectoryPath))
-            {
-                File.SetAttributes(filePath, FileAttributes.Normal);
-                File.Delete(filePath);
-            }
-            foreach (var directoryPath in Directory.GetDirectories(targetDirectoryPath))
-            {
-                DeleteDir(directoryPath);
-            }
+            Directory.GetFiles(targetDirectoryPath).Foreach(filePath => { File.SetAttributes(filePath, FileAttributes.Normal); });
+            Directory.GetFiles(targetDirectoryPath).Foreach(File.Delete);
+            Directory.GetDirectories(targetDirectoryPath).Foreach(DeleteDir);
             Directory.Delete(targetDirectoryPath, false);
         }
     }
