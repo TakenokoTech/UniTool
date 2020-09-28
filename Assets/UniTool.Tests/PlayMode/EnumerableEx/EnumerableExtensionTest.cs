b3943e5124ef5b88a265db47e91374701bdc8445
takenoko <takenoko.tech@gmail.com>
2020-09-29 02:13:03 +0900

    fix EnumEx.

--- Assets/UniTool.Tests/PlayMode/EnumerableEx/EnumerableExtensionTest.cs
+++ Assets/UniTool.Tests/PlayMode/EnumerableEx/EnumerableExtensionTest.cs
@@ -18,7 +18,7 @@ namespace UniTool.Tests.PlayMode.EnumerableEx
             Assert.AreEqual("A", indexedList[0].value);
             Assert.AreEqual("B", indexedList[1].value);
             Assert.AreEqual("C", indexedList[2].value);
-            
+
             var array = new[] {"A", "B", "C"};
             var indexedArray = array.WithIndex().ToArray();
             Assert.AreEqual(0, indexedList[0].index);
@@ -27,6 +27,56 @@ namespace UniTool.Tests.PlayMode.EnumerableEx
             Assert.AreEqual("A", indexedList[0].value);
             Assert.AreEqual("B", indexedList[1].value);
             Assert.AreEqual("C", indexedList[2].value);
+        }
+
+        [Test]
+        public void ForeachTest()
+        {
+            var actual1 = "";
+            var list = new List<string> {"A", "B", "C"};
+            list.Foreach(it => { actual1 += it; });
+            Assert.AreEqual("ABC", actual1);
+
+            var actual2 = "";
+            var array = new[] {"A", "B", "C"};
+            array.ForeachIndexed((index, value) => { actual2 += index + value; });
+            Assert.AreEqual("0A1B2C", actual2);
+        }
+
+        [Test]
+        public void MapTest()
+        {
+            var list = new List<string> {"A", null, "B", null, "C"};
+            var actual1 = list.Map(it => it + "1").ToList();
+            Assert.AreEqual("A1", actual1[0]);
+            Assert.AreEqual("1", actual1[1]);
+            Assert.AreEqual("B1", actual1[2]);
+            Assert.AreEqual("1", actual1[3]);
+            Assert.AreEqual("C1", actual1[4]);
+
+            var array = new[] {"A", null, "B", null, "C"};
+            var actual2 = array.MapIndexed((index, value) => value + index).ToArray();
+            Assert.AreEqual("A0", actual2[0]);
+            Assert.AreEqual("1", actual2[1]);
+            Assert.AreEqual("B2", actual2[2]);
+            Assert.AreEqual("3", actual2[3]);
+            Assert.AreEqual("C4", actual2[4]);
+        }
+
+        [Test]
+        public void FilterNotNullTest()
+        {
+            var list = new List<string> {"A", null, "B", null, "C"};
+            var actual1 = list.FilterNotNull().ToList();
+            Assert.AreEqual("A", actual1[0]);
+            Assert.AreEqual("B", actual1[1]);
+            Assert.AreEqual("C", actual1[2]);
+
+            var array = new[] {"A", null, "B", null, "C"};
+            var actual2 = array.FilterNotNull().ToArray();
+            Assert.AreEqual("A", actual2[0]);
+            Assert.AreEqual("B", actual2[1]);
+            Assert.AreEqual("C", actual2[2]);
         }
     }
 }
--- Assets/UniTool/EnumerableEx/EnumerableExtension.cs
+++ Assets/UniTool/EnumerableEx/EnumerableExtension.cs
@@ -9,12 +9,34 @@ namespace UniTool.EnumerableEx
     /// </summary>
     public static class EnumerableExtension
     {
-        public static IEnumerable<(int index, T value)> WithIndex<T>(this IEnumerable<T> source) =>
-            source.Select((t, i) => (i, t));
+        public static IEnumerable<(int index, T value)> WithIndex<T>(this IEnumerable<T> source)
+        {
+            return source.Select((t, i) => (i, t));
+        }
 
         public static void Foreach<T>(this IEnumerable<T> source, Action<T> block)
         {
             foreach (var t in source) block(t);
+        }
+        
+        public static void ForeachIndexed<T>(this IEnumerable<T> source, Action<int, T> block)
+        {
+            foreach (var (index, t) in source.WithIndex()) block(index, t);
+        }
+
+        public static IEnumerable<T2> Map<T1, T2>(this IEnumerable<T1> source, Func<T1, T2> block)
+        {
+            return source.Select(block);
+        }
+        
+        public static IEnumerable<T2> MapIndexed<T1, T2>(this IEnumerable<T1> source, Func<int, T1, T2> block)
+        {
+            return source.Select((t, index)  => block(index, t));
+        }
+        
+        public static IEnumerable<T> FilterNotNull<T>(this IEnumerable<T> source)
+        {
+            return source.Where(t => t != null);
         }
     }
 }
